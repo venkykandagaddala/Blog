70d252e9446bb11f01c1b725d85e015fc342bb43
venkatasubbaiah <venky.kandagaddala@gmail.com>
2013-03-22 10:02:40 +0530

    added Date of Birth and Location to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :firstname, :lastname, :middlename
+  attr_accessible :firstname, :lastname, :middlename, :dateofbirth, :location
   validates :firstname, :presence =>true,
 						:length => {:minimum => 5}
   validates :lastname, :presence =>true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130321120715) do
+ActiveRecord::Schema.define(:version => 20130322042230) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -43,9 +43,11 @@ ActiveRecord::Schema.define(:version => 20130321120715) do
   create_table "users", :force => true do |t|
     t.string   "firstname"
     t.string   "lastname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "middlename"
+    t.string   "dateofbirth"
+    t.string   "location"
   end
 
 end
